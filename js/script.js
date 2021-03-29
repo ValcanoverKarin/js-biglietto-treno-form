ea381ca569da8e65c2ea28020943b86afed8eee5
karin valcanover <valcanoverkarin@outlook.it>
2021-03-29 17:33:52 +0200

    inizio funzionalità del bottone genera

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// funzionalità del bottone genera
+var generateButton = document.getElementById('generate_button')
+generateButton.addEventListener('click', function() {
+    //operazioni da svolgere al click
+    //alert('click funziona')
+    var generatorNameElement = document.getElementById('generator_name');
+    var userName = generatorNameElement.value;
+    //console.log(userName);
+});
