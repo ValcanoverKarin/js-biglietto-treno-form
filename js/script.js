f7af3a9f1c644cfbcd3e4f432a496d42a0b02334
karin valcanover <valcanoverkarin@outlook.it>
2021-03-29 17:47:05 +0200

    offerta minorenne magiorenne over

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ var generateButton = document.getElementById('generate_button')
 generateButton.addEventListener('click', function() {
     //operazioni da svolgere al click
     //alert('click funziona')
+
+    //nome passeggero, leggo input generator_name
     var generatorNameElement = document.getElementById('generator_name');
     var userName = generatorNameElement.value;
     //console.log(userName);
+    
+    //offerta, leggo input generator_age, faccio if
+    // se maggiorenne, minorenne o over
+    var generatorAge = document.getElementById('generator_age');
+    var userAge = generatorAge.value;
+    //console.log(userAge);
+    var userOffer = 'Prezzo standard';
+    if ( userAge == 'minorenne') {
+        userOffer = 'Sconto minorenne';
+    } else if ( userAge == 'over') {
+        userOffer = 'Sconto over';
+    }
+    console.log(userOffer);
+
+    //carrozza
+    //codice
+    //prezzo
+
+    //stampare
 });
